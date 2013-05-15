b6c3d556444d57c8f250c336b2c717fb1955fb06
lorenzofox3 <laurent34azerty@gmail.com>
2013-05-15 21:36:47 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+/**
+ * User: laurentrenard
+ * Date: 5/14/13
+ * Time: 10:55 PM
+ **/
+var app=angular.module('app',['ui.bootstrap.tabs']);
---
+++ assets/js/controllers.js
@@ -0,0 +1,8 @@
+/**
+ * User: laurentrenard
+ * Date: 5/14/13
+ * Time: 10:55 PM
+ **/
+app.controller('mainCtrl',['$scope', function (scope) {
+    scope.greeting='hello Laurent';
+}]);
