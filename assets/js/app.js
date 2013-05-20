6b6d2df83ca4950882d743068bc6a9df1daf8ed6
lorenzofox3 <laurent34azerty@gmail.com>
2013-05-21 00:24:37 +0200

    added some chapters

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,17 @@
  * Date: 5/14/13
  * Time: 10:55 PM
  **/
-var app=angular.module('app',['ui.bootstrap.tabs']);
+var app=angular.module('app',['ui.bootstrap.tabs','SmartTable.Table']);
+app.directive('custom', function () {
+    return {
+        restrict:'E',
+        //include smart table controller to use its API if needed
+        require: '^smartTable',
+        template:'<div></div>',
+        replace:true,
+        link: function (scope, element, attrs, ctrl) {
+            //use the scope
+            element.text(scope.formatedValue.toUpperCase());
+        }
+    }
+});
--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -3,6 +3,151 @@
  * Date: 5/14/13
  * Time: 10:55 PM
  **/
-app.controller('mainCtrl',['$scope', function (scope) {
-    scope.greeting='hello Laurent';
+app.controller('mainCtrl', ['$scope', function (scope) {
+    scope.greeting = 'hello Laurent';
+}]);
+app.controller('basicsCtrl', ['$scope', function (scope) {
+    scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
+
+    scope.columnCollection = [
+        {label: 'First Name', map: 'firstName'},
+        {label: 'same same but different', map: 'firstName'},
+        {label: 'Last Name', map: 'lastName'}
+    ];
+}]);
+app.controller('formatCtrl', ['$scope', function (scope) {
+    scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
+
+    scope.columnCollection = [
+        {label: 'First Name', map: 'firstName', formatFunction: function (value, formatParameter) {
+            //this only display the first letter
+            return value[0];
+        }},
+        {label: 'Last Name', map: 'lastName', formatFunction: 'uppercase'},
+        {label: 'Birth Date', map: 'birthDate', formatFunction: 'date'},
+        {label: 'Balance', map: 'balance', formatFunction: 'currency', formatParameter: '$'}
+    ];
+}]);
+app.controller('sortCtrl', ['$scope', '$filter', function (scope, filter) {
+    scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
+
+    scope.columnCollection = [
+        {label: 'First Name', map: 'firstName', sortPredicate: function (dataRow) {
+            //predicate as a function (see angular orderby documentation) : it will sort by the string length
+            return dataRow.firstName.length;
+        } },
+        {label: 'Last Name', map: 'lastName', formatFunction: 'uppercase'},
+        {label: 'Birth Date', map: 'birthDate', formatFunction: 'date'},
+        {label: 'Balance', map: 'balance', formatFunction: 'currency', formatParameter: '$'},
+        {label: 'e-mail', map: 'email', isSortable: false}
+    ];
+
+//    scope.globalConfig = {
+//        sortAlgorithm: function (arrayRef, sortPredicate, reverse) {
+//            //it will sort all the column by length of
+//            return filter('orderBy')(arrayRef,null,reverse);
+//        }
+//    };
+
+}]);
+app.controller('filterCtrl', ['$scope', function (scope) {
+    scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
+
+    scope.columnCollection = [
+        {label: 'First Name', map: 'firstName', sortPredicate: function (dataRow) {
+            //predicate as a function (see angular orderby documentation) : it will sort by the string length
+            return dataRow.firstName.length;
+        } },
+        {label: 'Last Name', map: 'lastName', formatFunction: 'uppercase'},
+        {label: 'Birth Date', map: 'birthDate', formatFunction: 'date'},
+        {label: 'Balance', map: 'balance', formatFunction: 'currency', formatParameter: '$'},
+        {label: 'e-mail', map: 'email', isSortable: false}
+    ];
+    scope.globalConfig = {
+        isGlobalSearchActivated: true
+    };
+}]);
+app.controller('selectionCtrl', ['$scope', function (scope) {
+    scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
+
+    scope.columnCollection = [
+        {label: 'First Name', map: 'firstName'},
+        {label: 'Last Name', map: 'lastName', formatFunction: 'uppercase'},
+        {label: 'Birth Date', map: 'birthDate', formatFunction: 'date'},
+        {label: 'Balance', map: 'balance', formatFunction: 'currency', formatParameter: '$'},
+        {label: 'e-mail', map: 'email'}
+    ];
+    scope.globalConfig = {
+        selectionMode: 'multiple',
+        displaySelectionCheckbox: true
+    };
+
+    scope.canDisplayCheckbox = function () {
+        return scope.globalConfig.selectionMode === 'multiple';
+    };
+}]);
+app.controller('editCtrl', ['$scope', function (scope) {
+    scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
+
+    scope.columnCollection = [
+        {label: 'First Name', map: 'firstName', isEditable:true},
+        {label: 'Last Name', map: 'lastName', formatFunction: 'uppercase'},
+        {label: 'Birth Date', map: 'birthDate', formatFunction: 'date', isEditable:true, type:'date'},
+        {label: 'Balance', map: 'balance', formatFunction: 'currency', formatParameter: '$', isEditable:true, type:'number'},
+        {label: 'e-mail', map: 'email', isEditable:true, type:'email'}
+    ];
+}]);
+app.controller('styleCtrl', ['$scope', function (scope) {
+    scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
+
+    scope.columnCollection = [
+        {label: 'First Name', map: 'firstName', headerClass:"firstName-header"},
+        {label: 'Last Name', map: 'lastName', formatFunction: 'uppercase', cellClass:"lastName-cell"},
+        {label: 'Birth Date', map: 'birthDate', formatFunction: 'date'},
+        {label: 'Balance', map: 'balance', formatFunction: 'currency'},
+        {label: 'e-mail', map: 'email'}
+    ];
+}]);
+app.controller('cellTemplateCtrl', ['$scope', function (scope) {
+    scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
+
+    scope.columnCollection = [
+        {label: 'First Name', map: 'firstName',templateUrl:'assets/template/custom.html'},
+        {label: 'Last Name', map: 'lastName', formatFunction: 'uppercase'},
+        {label: 'Birth Date', map: 'birthDate', formatFunction: 'date'},
+        {label: 'Balance', map: 'balance', formatFunction: 'currency'},
+        {label: 'e-mail', map: 'email'}
+    ];
 }]);
