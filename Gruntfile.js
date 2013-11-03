c2f6e368df58e14ce5cbe22100d221353c032b44
lorenzofox3 <laurent34azerty@gmail.com>
2013-11-03 20:15:37 +0100

    typo

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,5 +47,5 @@ module.exports = function (grunt) {
         }
     });
     // Default task(s).
-    grunt.registerTask('default', ['concat', 'copy']);
+    grunt.registerTask('default', ['concat']);
 };
